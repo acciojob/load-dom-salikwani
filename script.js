2a3aef0f8dbb592527bd431c239e8e758cc4b97b
salikwani <120441251+salikwani@users.noreply.github.com>
2023-04-25 11:48:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //your JS code here. If required.
 window.addEventListener("DOMContentLoaded", ()=> {
-	document.body.innerText("DOM load success");
+	document.body.innerText="DOM load success";
 });
